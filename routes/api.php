61ef4175882b737b2f737079024aa859efdfaa4e
Antoine GREUZARD <antoin.greuzard@gmail.com>
2025-03-24 10:46:53 +0100

    fix: routes into middleware
    Signed-off-by: Antoine GREUZARD <antoin.greuzard@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,16 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->group(function () {
+// Routes PUBLIQUES
+Route::get('plants', [PlantController::class, 'index']);
+Route::get('plants/{plant}', [PlantController::class, 'show']);
+Route::get('plants/{plant}/photos', [PlantPhotoController::class, 'index']);
+
+// Routes PROTÉGÉES
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('plants', [PlantController::class, 'store']);
     Route::put('plants/{plant}', [PlantController::class, 'update']);
     Route::delete('plants/{plant}', [PlantController::class, 'destroy']);
+
     Route::post('plants/{plant}/photos', [PlantPhotoController::class, 'store']);
 });
