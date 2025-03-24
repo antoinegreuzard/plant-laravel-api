f22bc6d10fa105ea666ad324f501010e1e6c7128
Antoine GREUZARD <antoin.greuzard@gmail.com>
2025-03-24 10:49:27 +0100

    fix: routes into middleware
    Signed-off-by: Antoine GREUZARD <antoin.greuzard@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -5,20 +5,19 @@ use App\Http\Controllers\PlantPhotoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Routes PUBLIQUES
-Route::get('plants', [PlantController::class, 'index']);
-Route::get('plants/{plant}', [PlantController::class, 'show']);
-Route::get('plants/{plant}/photos', [PlantPhotoController::class, 'index']);
+Route::get('plants/', [PlantController::class, 'index'])->name('plant-list');
+Route::get('plants/{plant}/', [PlantController::class, 'show'])->name('plant-detail');
+Route::get('plants/{plant}/photos/', [PlantPhotoController::class, 'index'])->name('plant-photos-list');
 
-// Routes PROTÉGÉES
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('plants', [PlantController::class, 'store']);
-    Route::put('plants/{plant}', [PlantController::class, 'update']);
-    Route::delete('plants/{plant}', [PlantController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('plants/', [PlantController::class, 'store']);
+    Route::put('plants/{plant}/', [PlantController::class, 'update']);
+    Route::delete('plants/{plant}/', [PlantController::class, 'destroy']);
 
-    Route::post('plants/{plant}/photos', [PlantPhotoController::class, 'store']);
+    Route::post('plants/{plant}/upload-photo/', [PlantPhotoController::class, 'store'])
+        ->name('plant-upload-photo');
 });
