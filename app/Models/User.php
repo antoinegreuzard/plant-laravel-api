98e8f6cf63510a29ea12f8080970ef3d033b7dcf
Antoine GREUZARD <antoin.greuzard@gmail.com>
2025-04-04 10:32:36 +0200

    feat: user relation
    Signed-off-by: Antoine GREUZARD <antoin.greuzard@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
@@ -44,6 +45,11 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims(): array
     {
         return [];
+    }
+
+    public function plants(): HasMany
+    {
+        return $this->hasMany(Plant::class);
     }
 
     /**
